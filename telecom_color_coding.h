e2c8ce69fd4847c0ec223e9728ae6574e904d1d9
Amit2509 <amitsrivastava2509@gmail.com>
2022-08-14 02:26:26 +0530

    Update telecom_color_coding.h

--- telecom_color_coding.h
+++ telecom_color_coding.h
@@ -4,16 +4,12 @@
 enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
 enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
 
-const char* MajorColorNames[] = {
-    "White", "Red", "Black", "Yellow", "Violet"
-};
-int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+extern const char* MajorColorNames[]; 
+extern int numberOfMajorColors;
 
-const char* MinorColorNames[] = {
-    "Blue", "Orange", "Green", "Brown", "Slate"
-};
-const int MAX_COLORPAIR_NAME_CHARS = 16;
-int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+extern const char* MinorColorNames[];
+extern const int MAX_COLORPAIR_NAME_CHARS;
+extern int numberOfMinorColors;
 
 typedef struct {
     enum MajorColor majorColor;
