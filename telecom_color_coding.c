cec7ef23d7f69d1d877f201fba5e9be396c907ee
Amit2509 <amitsrivastava2509@gmail.com>
2022-08-14 22:59:59 +0530

    Update telecom_color_coding.c
    Adding print_color_coding_reference_manual() function definition

--- telecom_color_coding.c
+++ telecom_color_coding.c
@@ -31,3 +31,13 @@ int GetPairNumberFromColor(const ColorPair* colorPair) {
     return colorPair->majorColor * numberOfMinorColors +
             colorPair->minorColor + 1;
 }
+
+void print_color_coding_reference_manual()
+{
+    int i = 0, j = 0;
+    for(i = 0; i < numberOfMajorColors; i++) {
+        for(j = 0; j < numberOfMinorColors; j++) {
+            printf("%d | %s | %s\n", i * 5 + j + 1, majorColor[i], minorColor[j]);
+        }
+    }
+}
